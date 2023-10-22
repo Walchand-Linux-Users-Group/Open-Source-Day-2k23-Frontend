11896632fd0003eb66a96b7deeff269b0485a5fb
Smit Butle <smit2003.sb@gmail.com>
2023-10-22 15:38:52 +0530

    Updates

--- src/Mid.tsx
+++ src/Mid.tsx
@@ -9,9 +9,9 @@ const Mid = () => {
       </div>
       <img src="./midmob.png" alt="midmob" className={styles.mob}></img>
       <img src="./midpc.png" alt="midpc" className={styles.pc}></img>
-      <div className={styles.watch}>
+      {/* <div className={styles.watch}>
         <h1>Watch Trailer</h1>
-      </div>
+      </div> */}
       <div className={styles.reaper}>
         <div className={styles.text}>
           Walchand Linux Users' Group is a club of Linux and open source enthusiasts established in 2003 from Walchand College of Engineering, Sangli. We in WLUG make students explore all the aspects of FOSS with the huge community of knowledgeable and dedicated people. We conduct various activities throughout the year to promote open source technologies including talks, discussions, bootcamps and competitions.
